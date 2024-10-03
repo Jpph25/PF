0667cf472d7910ab4866bf544d501fd57404890b
João Gomes <jppgomes06@gmail.com>
2024-10-03 16:44:26 +0100

    Translation to English

--- Fichas/ficha1.hs
+++ Fichas/ficha1.hs
@@ -3,48 +3,44 @@
 --  1
 
 -- a)
--- Calculo do perimetro de uma circunferênci
-perimetro :: Double -> Double
-perimetro r = 2 * pi * r 
+-- Perimeter if a circunference
+perimeter :: Double -> Double
+perimeter r = 2 * pi * r 
 
 -- b)
--- Calculo da distância entre dois pontos
+-- Distance of two points 
 dist :: (Double,Double) -> (Double,Double) -> Double
 dist (x1,y1) (x2,y2) = sqrt ((x1-x2)^2 + (y1-y2)^2)
 
 -- c)
--- Lista do primeiro e ultimo elemento de uma lista
-primUlt :: [a] -> (a,a)
-primUlt l = (head l,last l)
+-- Creates pair of the first and last element of a list
+fstUlt :: [a] -> (a,a)
+fstUlt l = (head l,last l)
 
 -- d)
--- Verifica se um numero é multiplo de outro
-multiplo :: Int -> Int -> String  
-multiplo m n = if   mod n m == 0 
-               then "True"
-               else "False"  
-
-
-multiplo' :: Int -> Int -> Bool 
-multiplo' m n = mod n m == 0 
+-- Verifies if the first number is a multiple of the second  
+multiple :: Int -> Int -> Bool  
+multiple m n = if   mod n m == 0 
+               then True
+               else False 
 
 -- e)
--- Devolve a lista caso a quantidade de elemnetos seja par 
--- e devolve tudo menos o primeiro elemento caso seja impar
-truncaImpar :: [a] -> [a]
-truncaImpar l = if   even (length l) 
+-- If the legth of the list is even gives the list 
+-- if not takes the first element of the list 
+truncaOdd :: [a] -> [a]
+truncaOdd l = if   even (length l) 
                 then l 
                 else tail l   
 
 -- f)
--- Diz qual é o maior numero entre dois numeros
+-- Wich nun«mber is bigger between two numbers
 max2 :: Int -> Int -> Int 
 max2 a b = if   a > b
            then a
            else b 
 
 -- g)
--- Diz qual é o maior numero entre três numeros
+-- Wich nun«mber is bigger between three numbers
 max3 :: Int -> Int -> Int -> Int 
 max3 a b c = if  (max2 a b) > c 
              then max2 a b 
@@ -58,21 +54,21 @@ max3' a b c = if   a > b
 --  2
 
 -- a)
--- Diz quantas raizes um polinomio de grau 2 têm 
-nRaizes :: Float -> Float -> Float -> String
-nRaizes a b c = if   b^2 - 4 * c * a == 0 
-                then "O polinomio tem uma raiz"
+-- How many roots a equation has 
+nRoots :: Float -> Float -> Float -> String
+nRoots a b c = if   b^2 - 4 * c * a == 0 
+                then "The polynomial has one root"
                 else if   b^2 - 4 * c * a > 0 
-                     then "O polinomio tem duas raizes"
-                     else "O polinomio nao tem raizes"                                                        
+                     then "The polynomial has two roots"
+                     else "The polynomial has no root"                                                        
 
 -- b)
--- Calcula as raizes de um polinomio 
-raizes :: Float -> Float -> Float -> [Float]
-raizes a b c = if   nRaizes a b c == "O polinomio tem duas raizes"
+-- Calculates the roots of the polynomials  
+roots :: Float -> Float -> Float -> [Float]
+roots a b c = if   nRoots a b c == "The polynomial has two roots"
                then [(-b + sqrt (b * b - 4 * c * a)) / (2 * a),
                      (-b - sqrt (b * b - 4 * c * a)) / (2 * a) ]
-               else if   nRaizes a b c == "O polinomio tem uma raiz" 
+               else if   nRoots a b c == "The polynomial has one root"  
                     then [(-b) / (2 * a) ]
                     else [] 
 
@@ -81,40 +77,36 @@ raizes a b c = if   nRaizes a b c == "O polinomio tem duas raizes"
 type Hora = (Int,Int)
 
 -- a)
--- Verifica se uma hora é valida
-horaValid :: Hora -> Bool
-horaValid (h,m) =    h >= 0 && h < 24 
+-- Verifies if its a valid hour 
+validHour :: Hora -> Bool
+validHour (h,m) =    h >= 0 && h < 24 
                   && m >= 0 && m < 60 
                
 -- b)
--- Diz se a primeira hora é depois da segunda hora
-horaOrd :: Hora -> Hora -> Bool
-horaOrd (h1,m1) (h2,m2) = if   (   horaValid (h1,m1) 
-                                && horaValid (h2,m2)) == True
-                          then if   h1 > h2 
-                               then True
-                               else if   h1 == h2 
-                                    then m1 > m2 
-                                    else False     
-                          else False      
+-- Says if an hour is after another
+ordHour :: Hora -> Hora -> Bool
+ordHour (h1,m1) (h2,m2) 
+                        | h1 > h2 = True 
+                        | h1 < h2 && m1 < m2 = True 
+                        | otherwise = False
 
 -- c) 
--- Converte horas em minutos 
+-- Convert hours into minutes
 convHtoM :: Hora -> Int 
 convHtoM (h,m) = h * 60 + m  
 
 -- d)
--- Converte minutos em horas
+-- Convert minutes into hours 
 convMtoH :: Int -> Hora
 convMtoH m = (div m 60, m - 60 * div m 60)
 
 -- e) 
--- Diferença entre duas horas (em minutos)
+-- Diference between two hours (in minutes) 
 diffHoras :: Hora -> Hora -> Int
 diffHoras (h1,m1) (h2,m2) = abs(convHtoM (h1,m1) - convHtoM(h2,m2)) 
 
 -- f) 
--- Adiciona minutos a um par de horas
+-- Adds minutes to an hour 
 addMtoH :: Int -> Hora -> Hora
 addMtoH m (h1,m1) = convMtoH(m + convHtoM(h1,m1)) 
 
@@ -124,15 +116,15 @@ data Hora' = H Int Int
            deriving (Show,Eq)
 
 -- a) 
--- Verifica se uma hora é valida
-horaValid' :: Hora' -> Bool
-horaValid' (H h m) = if    h >= 0 && h < 24 
+-- Verifies if its a valid hour 
+validHour' :: Hora' -> Bool
+validHour' (H h m) = if    h >= 0 && h < 24 
                         && m >= 0 && m < 60 
                      then True
                      else False
 
 -- b)
--- Testa se a primeira hora é depois da segunda hora  
+-- Says if an hour is after another 
 horaOrd' :: Hora' -> Hora' -> Bool
 horaOrd' (H h1 m1) (H h2 m2) 
                             | h1 > h2 = True 
@@ -140,22 +132,22 @@ horaOrd' (H h1 m1) (H h2 m2)
                             | otherwise = False 
 
 -- c) 
--- Coverte horas em minutos 
+-- Convert hours into minutes 
 convHtoM' :: Hora' -> Int 
 convHtoM' (H h m) = h * 60 + m 
   
 -- d)
--- Converte minutos em horas
+-- Convert minutes into hours 
 convMtoH' :: Int -> Hora'  
 convMtoH' m = (H (div m 60) (m - 60 * div m 60))                                  
 
 -- e) 
--- Calcula a diferença entre duas horas 
+-- Diference between two hours (in minutes) 
 diffHoras' :: Hora' -> Hora' -> Int   
 diffHoras' (H h1 m1) (H h2 m2) = abs(convHtoM'(H h1 m1) - convHtoM'(H h2 m2))   
 
 -- f) 
--- Adiciona minutos a uma dada hora 
+-- Adds minutes to an hour 
 addMtoH' :: Int -> Hora' -> Hora' 
 addMtoH' m (H h1 m1) = convMtoH'(m + convHtoM'(H h1 m1))  
 
@@ -167,7 +159,7 @@ data Semaforo =  Verde
               deriving (Show,Eq)  
 
 -- a)
--- Calcula a proxima cor do semafro 
+-- Says the enxt colour of the Traffic light 
 next :: Semaforo -> Semaforo
 next s 
       | s == Verde   = Amarelo
@@ -180,7 +172,7 @@ next' Amarelo  = Vermelho
 next' Vermelho = Verde
 
 -- b) 
--- Indica se é obrigatorio parar
+-- Says if its obligatorie to stop  
 stop :: Semaforo -> Bool
 stop s = if   s == Vermelho 
          then True
@@ -189,3 +181,8 @@ stop s = if   s == Vermelho
 stop' :: Semaforo -> Bool 
 stop' Vermelho = True 
 stop' _        = False   
+
+-- c) 
+-- Test if the Traffic light in a crossroad 
+safe :: Semaforo -> Semaforo -> Bool
+safe 
